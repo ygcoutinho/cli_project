ed8d38cda7f8a51b46d2d0a9c0094083dfe9ad27
Yago Coutinho <yagogc.it@gmail.com>
2023-01-23 20:49:14 -0300

    preparo do projeto

--- bin/cli_project.dart
+++ bin/cli_project.dart
@@ -1,5 +1,3 @@
 import 'package:cli_project/cli_project.dart' as cli_project;
 
-void main(List<String> arguments) {
-  print('Hello world: ${cli_project.calculate()}!');
-}
+void main(List<String> arguments) {}
