e2f6768319f3a6691bcb5a9d68e3de1a22a4655f
Yago Coutinho <yagogc.it@gmail.com>
2023-02-02 16:53:23 -0300

    Atualização

---
+++ lib/src/commands/students/subcommands/find_all_command.dart
@@ -0,0 +1,11 @@
+import 'package:args/command_runner.dart';
+
+class FindAllCommand extends Command {
+  @override
+  // TODO: implement description
+  String get description => throw UnimplementedError();
+
+  @override
+  // TODO: implement name
+  String get name => throw UnimplementedError();
+}
