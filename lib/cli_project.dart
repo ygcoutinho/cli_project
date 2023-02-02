8329f2f069cbc3cbe3466c1b1099cacd4e81d727
Yago Coutinho <yagogc.it@gmail.com>
2023-02-02 15:26:54 -0300

    repositories

--- lib/cli_project.dart
+++ lib/cli_project.dart
@@ -1,8 +1,15 @@
+import 'models/course.dart';
 import 'models/student.dart';
+import 'repositories/product_repository.dart';
 import 'repositories/students_repository.dart';
 
 Future<void> main() async {
   var studentsRepository = StudentsRepository();
-  List<Student> listStudents = await studentsRepository.findAll();
-  print(listStudents);
+  var productsRepository = ProductRepository();
+  //List<Student> listStudents = await studentsRepository.findAll();
+  //print(listStudents);
+  var listCourses = await productsRepository.findByName("Jornada Dart");
+  print(listCourses);
+  // Student studentById = await studentsRepository.findById(12);
+  // print(studentById);
 }
